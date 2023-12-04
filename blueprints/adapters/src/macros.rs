eb6f1eca939cb77fb3c64e7ace62206c878485cf
Omar <OmarAbdulla7@hotmail.com>
2023-12-04 18:22:35 +0300

    Limit adapters to just method calls - they're always stateful.

--- blueprints/adapters/src/macros.rs
+++ blueprints/adapters/src/macros.rs
@@ -8,108 +8,36 @@ macro_rules! define_adapter_stubs {
             $($functions_tokens: tt)*
         ]
     ) => {
-        /* Scrypto stub */
         #[cfg(feature = "scrypto")]
         pub use scrypto::$adapter_name;
 
+        /* Scrypto stubs */
         #[cfg(feature = "scrypto")]
         mod scrypto {
             #[derive(::scrypto::prelude::ScryptoSbor)]
             #[sbor(transparent)]
-            pub struct $adapter_name<T>(pub T);
+            pub struct $adapter_name(pub ::scrypto::prelude::Reference);
 
-            impl<T> From<T> for $adapter_name<::scrypto::prelude::BlueprintId>
-            where
-                T: Into<::scrypto::prelude::BlueprintId>,
-            {
-                fn from(value: T) -> $adapter_name<::scrypto::prelude::BlueprintId> {
-                    Self(value.into())
-                }
-            }
-
-            impl<T> From<T> for $adapter_name<::scrypto::prelude::Reference>
+            #[cfg(feature = "scrypto")]
+            impl<T> From<T> for $adapter_name
             where
                 T: Into<::scrypto::prelude::NodeId>,
             {
-                fn from(value: T) -> $adapter_name<::scrypto::prelude::Reference> {
+                fn from(value: T) -> Self {
                     Self(::scrypto::prelude::Reference(value.into()))
                 }
             }
 
-            impl $adapter_name<::scrypto::prelude::BlueprintId> {
+            #[cfg(feature = "scrypto")]
+            impl $adapter_name {
                 $crate::define_functions!($($functions_tokens)*);
             }
-
-            impl $adapter_name<::scrypto::prelude::Reference> {
-                $crate::define_methods!($($functions_tokens)*);
-            }
         }
-
-
     };
 }
 
 #[macro_export]
 macro_rules! define_functions {
-    (
-        $(#[$meta: meta])*
-        fn $fn_name: ident (
-            $(
-                $arg_name: ident: $arg_type: ty
-            ),* $(,)?
-        ) $(-> $rtn_type: ty)? ; $($tokens: tt)*
-    ) => {
-        $(#[$meta])*
-        pub fn $fn_name(
-            $( $arg_name: $arg_type, )*
-        ) -> resolve_return_type!($(-> $rtn_type)?) {
-            ::scrypto::prelude::scrypto_decode(&::scrypto::prelude::ScryptoVmV1Api::blueprint_call(
-                self.0.package_address,
-                &self.0.blueprint_name,
-                stringify!($fn_name),
-                ::scrypto::prelude::scrypto_encode(&(
-                    $( $arg_name ),* ,
-                )).unwrap()
-            )).unwrap()
-        }
-    };
-    (
-        $(#[$meta:meta])*
-        fn $fn_name: ident (
-            &self,
-            $(
-                $arg_name: ident: $arg_type: ty
-            ),* $(,)?
-        ) $(-> $rtn_type: ty)? ; $($tokens: tt)*
-    ) => {
-
-    };
-    (
-        $(#[$meta:meta])*
-        fn $fn_name: ident (
-            &mut self,
-            $(
-                $arg_name: ident: $arg_type: ty
-            ),* $(,)?
-        ) $(-> $rtn_type: ty)? ; $($tokens: tt)*
-    ) => {
-
-    };
-    () => {};
-}
-
-#[macro_export]
-macro_rules! define_methods {
-    (
-        $(#[$meta: meta])*
-        fn $fn_name: ident (
-            $(
-                $arg_name: ident: $arg_type: ty
-            ),* $(,)?
-        ) $(-> $rtn_type: ty)? ; $($tokens: tt)*
-    ) => {
-
-    };
     (
         $(#[$meta:meta])*
         fn $fn_name: ident (
