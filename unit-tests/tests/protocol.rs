1822931a1fdc842d47e6d3d57d26cd67e13ef8a3
Omar <OmarAbdulla7@hotmail.com>
2024-01-03 15:05:43 +0300

    Add test: can't open a liquidity position when opening is disabled

--- unit-tests/tests/protocol.rs
+++ unit-tests/tests/protocol.rs
@@ -27,6 +27,38 @@ fn cant_add_a_pool_with_no_corresponding_adapter() -> Result<(), RuntimeError> {
 }
 
 #[test]
+fn cant_open_a_liquidity_position_when_opening_is_disabled(
+) -> Result<(), RuntimeError> {
+    // Arrange
+    let Environment {
+        environment: ref mut env,
+        mut resources,
+        mut protocol,
+        ociswap,
+    } = Environment::new()?;
+    protocol
+        .olympus
+        .config_open_liquidity_position(false, env)?;
+
+    // Act
+    let bitcoin_contribution_amount = dec!(0.1);
+    let bitcoin_bucket = resources
+        .bitcoin
+        .mint_fungible(bitcoin_contribution_amount, env)?;
+    let rtn = protocol.olympus.open_liquidity_position(
+        ociswap.bitcoin_pool.try_into().unwrap(),
+        FungibleBucket(bitcoin_bucket),
+        LockupPeriod::from_months(6),
+        env,
+    );
+
+    // Assert
+    assert!(is_wasm_panic(&rtn));
+
+    Ok(())
+}
+
+#[test]
 fn can_open_position_on_an_ociswap_pool() -> Result<(), RuntimeError> {
     // Arrange
     let Environment {
