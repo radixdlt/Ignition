74a1185c48f5faec9cab6b35c6b3333ee9584cc9
Omar <OmarAbdulla7@hotmail.com>
2024-01-11 17:23:24 +0300

    [Caviarnine Adapter V1]: Initial implementation of adapter

---
+++ packages/caviarnine-adapter-v1/src/blueprint_interface.rs
@@ -0,0 +1,70 @@
+use scrypto::prelude::*;
+use scrypto_interface::*;
+
+define_interface! {
+    QuantaSwap as CaviarNinePool impl [ScryptoStub, ScryptoTestStub] {
+        fn new(
+            owner_rule: AccessRule,
+            user_rule: AccessRule,
+            token_x_address: ResourceAddress,
+            token_y_address: ResourceAddress,
+            bin_span: u32,
+            reservation: Option<GlobalAddressReservation>,
+        ) -> Self;
+        fn get_fee_controller_address(&self) -> ComponentAddress;
+        fn get_fee_vaults_address(&self) -> ComponentAddress;
+        fn get_token_x_address(&self) -> ResourceAddress;
+        fn get_token_y_address(&self) -> ResourceAddress;
+        fn get_liquidity_receipt_address(&self) -> ResourceAddress;
+        fn get_bin_span(&self) -> u32;
+        fn get_amount_x(&self) -> Decimal;
+        fn get_amount_y(&self) -> Decimal;
+        fn get_active_tick(&self) -> Option<u32>;
+        fn get_price(&self) -> Option<Decimal>;
+        fn get_active_bin_price_range(&self) -> Option<(Decimal, Decimal)>;
+        fn get_active_amounts(&self) -> Option<(Decimal, Decimal)>;
+        fn get_bins_above(
+            &self,
+            start_tick: Option<u32>,
+            stop_tick: Option<u32>,
+            number: Option<u32>,
+        ) -> Vec<(u32, Decimal)>;
+        fn get_bins_below(
+            &self,
+            start_tick: Option<u32>,
+            stop_tick: Option<u32>,
+            number: Option<u32>,
+        ) -> Vec<(u32, Decimal)>;
+        fn get_liquidity_claims(
+            &self,
+            liquidity_receipt_id: NonFungibleLocalId,
+        ) -> IndexMap<u32, Decimal>;
+        fn get_redemption_value(&self, liquidity_receipt_id: NonFungibleLocalId) -> (Decimal, Decimal);
+        fn get_redemption_bin_values(
+            &self,
+            liquidity_receipt_id: NonFungibleLocalId,
+        ) -> Vec<(u32, Decimal, Decimal)>;
+        fn mint_liquidity_receipt(&mut self) -> Bucket;
+        fn burn_liquidity_receipt(&mut self, liquidity_receipt: Bucket);
+        fn add_liquidity_to_receipt(
+            &mut self,
+            liquidity_receipt: Bucket,
+            tokens_x: Bucket,
+            tokens_y: Bucket,
+            positions: Vec<(u32, Decimal, Decimal)>,
+        ) -> (Bucket, Bucket, Bucket);
+        fn add_liquidity(
+            &mut self,
+            tokens_x: Bucket,
+            tokens_y: Bucket,
+            positions: Vec<(u32, Decimal, Decimal)>,
+        ) -> (Bucket, Bucket, Bucket);
+        fn remove_specific_liquidity(
+            &mut self,
+            liquidity_receipt: Bucket,
+            claims: Vec<(u32, Decimal)>,
+        ) -> (Bucket, Bucket, Bucket);
+        fn remove_liquidity(&mut self, liquidity_receipt: Bucket) -> (Bucket, Bucket);
+        fn swap(&mut self, tokens: Bucket) -> (Bucket, Bucket);
+    }
+}
---
+++ packages/caviarnine-adapter-v1/src/lib.rs
@@ -0,0 +1,171 @@
+mod bin_selector;
+mod blueprint_interface;
+
+pub use crate::bin_selector::*;
+pub use crate::blueprint_interface::*;
+
+use adapters_interface::prelude::*;
+use scrypto::prelude::*;
+use scrypto_interface::*;
+
+#[blueprint_with_traits]
+pub mod adapter {
+    struct CaviarNineAdapter;
+
+    impl CaviarNineAdapter {
+        fn pool(
+            component_address: ComponentAddress,
+        ) -> CaviarNinePoolInterfaceScryptoStub {
+            CaviarNinePoolInterfaceScryptoStub::from(component_address)
+        }
+    }
+
+    impl PoolAdapterInterfaceTrait for CaviarNineAdapter {
+        fn open_liquidity_position(
+            &mut self,
+            pool_address: ComponentAddress,
+            buckets: (Bucket, Bucket),
+        ) -> OpenLiquidityPositionOutput {
+            let mut pool = Self::pool(pool_address);
+
+            // Split the two buckets into bucket_x and bucket_y in the same way
+            // that they're defined in the pool itself.
+            let resource_address_x = pool.get_token_x_address();
+            let resource_address_y = pool.get_token_y_address();
+            let (bucket_x, bucket_y) = if buckets.0.resource_address()
+                == resource_address_x
+                && buckets.1.resource_address() == resource_address_y
+            {
+                (buckets.0, buckets.1)
+            } else if buckets.1.resource_address() == resource_address_x
+                && buckets.0.resource_address() == resource_address_y
+            {
+                (buckets.1, buckets.0)
+            } else {
+                panic!("One or more of the buckets do not belong to the pool")
+            };
+            let amount_x = bucket_x.amount();
+            let amount_y = bucket_y.amount();
+
+            // Select the bins that we will contribute to.
+            let bin_span = pool.get_bin_span();
+            let active_bin =
+                pool.get_active_tick().expect("Pool has no active bin!");
+            let SelectedBins {
+                higher_bins,
+                lower_bins,
+                ..
+            } = SelectedBins::select(active_bin, bin_span, 198);
+
+            // Determine the amount of resources that we will add to each of the
+            // bins. We have 99 on the left and 99 on the right. But, we also
+            // have the active bin that is composed of both x and y. So, this
+            // be like contributing to 99.x and 99.y bins where x = 1-y. X here
+            // is the ratio of resources x in the active bin.
+            let (amount_in_active_bin_x, amount_in_active_bin_y) =
+                pool.get_active_amounts().expect("No active amounts");
+            let price = pool.get_price().expect("No price");
+
+            let ratio_in_active_bin_x = amount_in_active_bin_x * price
+                / (amount_in_active_bin_x * price + amount_in_active_bin_y);
+            let ratio_in_active_bin_y = Decimal::one() - ratio_in_active_bin_x;
+
+            // In here, we decide the amount x by the number of higher bins plus
+            // the ratio of the x in the currently active bin since the pool
+            // starting from the current price and upward is entirely composed
+            // of X. Similarly, we divide amount_y by the number of lower
+            // positions plus the ratio of y in the active bin since the pool
+            // starting from the current price and downward is composed just of
+            // y.
+            let position_amount_x = amount_x
+                / (Decimal::from(higher_bins.len() as u32)
+                    + ratio_in_active_bin_x);
+            let position_amount_y = amount_y
+                / (Decimal::from(lower_bins.len() as u32)
+                    + ratio_in_active_bin_y);
+
+            // TODO: What?
+            let amount_bin_x_in_y = position_amount_x * price;
+            let (position_amount_x, position_amount_y) =
+                if amount_bin_x_in_y > position_amount_y {
+                    let position_amount_y_in_x = position_amount_y / price;
+                    (position_amount_y_in_x, position_amount_y)
+                } else {
+                    (position_amount_x, amount_bin_x_in_y)
+                };
+
+            let mut positions = vec![(
+                active_bin,
+                position_amount_x * ratio_in_active_bin_x,
+                position_amount_y * ratio_in_active_bin_y,
+            )];
+            positions.extend(
+                lower_bins
+                    .iter()
+                    .map(|bin_id| (*bin_id, dec!(0), position_amount_y)),
+            );
+            positions.extend(
+                lower_bins
+                    .iter()
+                    .map(|bin_id| (*bin_id, position_amount_x, dec!(0))),
+            );
+
+            let (receipt, change_x, change_y) =
+                pool.add_liquidity(bucket_x, bucket_y, positions);
+
+            OpenLiquidityPositionOutput {
+                pool_units: receipt,
+                change: indexmap! {
+                    change_x.resource_address() => change_x,
+                    change_y.resource_address() => change_y,
+                },
+                others: vec![],
+            }
+        }
+
+        fn close_liquidity_position(
+            &mut self,
+            pool_address: ComponentAddress,
+            pool_units: Bucket,
+        ) -> CloseLiquidityPositionOutput {
+            let mut pool = Self::pool(pool_address);
+
+            let (bucket1, bucket2) = pool.remove_liquidity(pool_units);
+
+            CloseLiquidityPositionOutput {
+                resources: indexmap! {
+                    bucket1.resource_address() => bucket1,
+                    bucket2.resource_address() => bucket2,
+                },
+                others: Default::default(),
+                // TODO: Determine how we wish to go about this calculation.
+                fees: Default::default(),
+            }
+        }
+
+        fn price(&mut self, pool_address: ComponentAddress) -> Price {
+            let pool = Self::pool(pool_address);
+
+            let (resource_address_x, resource_address_y) =
+                self.resource_addresses(pool_address);
+            let price = pool
+                .get_price()
+                .expect("[CaviarNine Adapter]: Price must be defined");
+
+            Price {
+                base: resource_address_x,
+                quote: resource_address_y,
+                price,
+            }
+        }
+
+        fn resource_addresses(
+            &mut self,
+            pool_address: ComponentAddress,
+        ) -> (ResourceAddress, ResourceAddress) {
+            let pool = Self::pool(pool_address);
+
+            (pool.get_token_x_address(), pool.get_token_y_address())
+        }
+    }
+}
