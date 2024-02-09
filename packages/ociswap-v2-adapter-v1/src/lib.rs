e286052ffe5f11ffc340eecb23b4944c16e7628f
Omar <OmarAbdulla7@hotmail.com>
2024-02-09 14:57:19 -0800

    [Ociswap v2 Adapter v1]: Add an adapter for ociswap v2

---
+++ packages/ociswap-v2-adapter-v1/src/lib.rs
@@ -0,0 +1,109 @@
+#![warn(clippy::arithmetic_side_effects)]
+
+mod blueprint_interface;
+pub use blueprint_interface::*;
+
+use ports_interface::prelude::*;
+use scrypto::prelude::*;
+use scrypto_interface::*;
+
+macro_rules! define_error {
+    (
+        $(
+            $name: ident => $item: expr;
+        )*
+    ) => {
+        $(
+            pub const $name: &'static str = concat!("[Ociswap v2 Adapter v1]", " ", $item);
+        )*
+    };
+}
+
+define_error! {
+    FAILED_TO_GET_RESOURCE_ADDRESSES_ERROR
+        => "Failed to get resource addresses - unexpected error.";
+    FAILED_TO_GET_VAULT_ERROR
+        => "Failed to get vault - unexpected error.";
+    PRICE_IS_UNDEFINED
+        => "Price is undefined.";
+    FAILED_TO_CALCULATE_K_VALUE_OF_POOL_ERROR
+        => "Failed to calculate the K value of the pool.";
+    OVERFLOW_ERROR => "Calculation overflowed.";
+}
+
+#[blueprint_with_traits]
+pub mod adapter {
+    struct OciswapV2Adapter;
+
+    impl OciswapV2Adapter {
+        pub fn instantiate(
+            metadata_init: MetadataInit,
+            owner_role: OwnerRole,
+            address_reservation: Option<GlobalAddressReservation>,
+        ) -> Global<OciswapV2Adapter> {
+            let address_reservation = address_reservation.unwrap_or(
+                Runtime::allocate_component_address(BlueprintId {
+                    package_address: Runtime::package_address(),
+                    blueprint_name: Runtime::blueprint_name(),
+                })
+                .0,
+            );
+
+            Self {}
+                .instantiate()
+                .prepare_to_globalize(owner_role)
+                .metadata(ModuleConfig {
+                    init: metadata_init,
+                    roles: Default::default(),
+                })
+                .with_address(address_reservation)
+                .globalize()
+        }
+
+        fn pool(
+            component_address: ComponentAddress,
+        ) -> OciswapV2PoolInterfaceScryptoStub {
+            OciswapV2PoolInterfaceScryptoStub::from(component_address)
+        }
+    }
+
+    impl PoolAdapterInterfaceTrait for OciswapV2Adapter {
+        fn open_liquidity_position(
+            &mut self,
+            _pool_address: ComponentAddress,
+            _buckets: (Bucket, Bucket),
+        ) -> OpenLiquidityPositionOutput {
+            todo!()
+        }
+
+        fn close_liquidity_position(
+            &mut self,
+            _pool_address: ComponentAddress,
+            _pool_units: Bucket,
+            _adapter_specific_information: AnyValue,
+        ) -> CloseLiquidityPositionOutput {
+            todo!()
+        }
+
+        fn price(&mut self, _pool_address: ComponentAddress) -> Price {
+            todo!()
+        }
+
+        fn resource_addresses(
+            &mut self,
+            pool_address: ComponentAddress,
+        ) -> (ResourceAddress, ResourceAddress) {
+            let pool = Self::pool(pool_address);
+            (pool.x_address(), pool.y_address())
+        }
+    }
+}
+
+#[derive(ScryptoSbor, Debug, Clone)]
+pub struct OciswapV2AdapterSpecificInformation {}
+
+impl From<OciswapV2AdapterSpecificInformation> for AnyValue {
+    fn from(value: OciswapV2AdapterSpecificInformation) -> Self {
+        AnyValue::from_typed(&value).unwrap()
+    }
+}
